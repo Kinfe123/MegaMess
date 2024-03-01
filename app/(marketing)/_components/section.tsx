7b0637c440335249b48ef11388e3315e9d69b3e2
Kinfe123 <kinfishtech@gmail.com>
2024-03-02 00:05:26 +0300

    fix: bg fix

--- app/(marketing)/_components/section.tsx
+++ app/(marketing)/_components/section.tsx
@@ -16,18 +16,19 @@ const Section = ({
       ${
         customPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+
       } 
       ${className || ""}`}
     >
       {children}
 
-      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
-      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
+      <div className="hidden absolute top-[-30px] left-5 w-[0.0625rem] h-full bg-[#26242C]  pointer-events-none md:block lg:left-7.5 xl:left-10" />
+      <div className="hidden absolute top-[-30px] right-5 w-[0.0625rem] h-full bg-[#26242C] pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
       {crosses && (
         <>
           <div
-            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
+            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-[#26242C] ${
               crossesOffset && crossesOffset
             } pointer-events-none lg:block xl:left-10 right-10`}
           />
