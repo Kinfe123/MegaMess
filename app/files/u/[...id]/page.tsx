28974d841e815bda16fede42cccf4540b75586df
Kinfe123 <kinfishtech@gmail.com>
2024-03-03 15:01:30 +0300

    fix: file and user helper added

--- app/files/u/[...id]/page.tsx
+++ app/files/u/[...id]/page.tsx
@@ -1,6 +1,14 @@
+import { fileInfo, findUserByFile } from "@/lib/file-info"
 
-
-const FilePreview = async () => {
+type FilePreviewProps = {
+    params: {
+        id: string[]
+    }
+}
+const FilePreview = async ({params}: FilePreviewProps) => {
+    const fullUrl = fileInfo(params.id[0])
+    const user = await findUserByFile(fullUrl)
+    
     return (
         <div>
             hllo world
--- lib/file-info.ts
+++ lib/file-info.ts
@@ -1,3 +1,21 @@
-export const filInfo = async () => {
-    const baseUrl = 
+import { env } from "process"
+import { prisma } from "./db"
+
+export const fileInfo = (id: string) => {
+    const baseUrl = env.BASE_EDGESTORE_URL
+    const fullUrl = `${baseUrl}/id`
+    return fullUrl
+}
+
+export const findUserByFile =  async (fileUrl: string) => {
+    const user = await prisma.file.findFirst({
+        where: {
+            fileUrl: fileUrl
+        },
+        include: {
+            user: true
+        }
+    })
+    console.log('TH user is: ' , user)
+
 }
