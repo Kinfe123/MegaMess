c0a4925098ed5590f3a9385740054c8921990e39
Kinfe123 <kinfishtech@gmail.com>
2024-03-03 17:36:52 +0300

    fix: partial fixed on /f/files

--- app/files/f/[...id]/_components/empty-box.tsx
+++ app/files/f/[...id]/_components/empty-box.tsx
@@ -1,6 +1,10 @@
 import { EmptyPlaceholder } from "@/components/shared/empty-placeholder"
 import { type File } from "@prisma/client"
 import { BodySkeleton } from "../page";
+import { timeAgo } from "@/lib/utils";
+import { Check, Download } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from 'next/link'
 type FilePromiseProps = {
     file: Promise<({ user: { image: string | null; name: string | null; }; } & File) | null>
 
@@ -14,12 +18,31 @@ const FileDescription = async ({ file }: FilePromiseProps) => {
             <EmptyPlaceholder className="bg-gradient-to-tr from-purple-400/10 rounded-md  via-transparent to-transparent/5 w-full flex justify-start ">
                 <EmptyPlaceholder.Icon name="post" />
 
-                    <EmptyPlaceholder.Title className="font-heading text-3xl"> <span className='text-gradient_indigo-purple font-extrabold'>{files?.name}</span></EmptyPlaceholder.Title>
-                    <EmptyPlaceholder.Description>
-                        {files?.description}
-                    </EmptyPlaceholder.Description>
-                    <div className='flex justify-center items-center mx-auto'>
+                <EmptyPlaceholder.Title className="font-heading text-3xl"> <span className='text-gradient_indigo-purple font-extrabold'>{files?.name}</span></EmptyPlaceholder.Title>
+                <EmptyPlaceholder.Description>
+                    {files?.description}
+                </EmptyPlaceholder.Description>
+                <div className='flex font-urban justify-start items-start flex-col  mx-auto f'>
+                    <div className="flex gap-2 justify-center items-center">
 
+                        <Check className="w-4 h-4" />  <p className='text-muted-foreground '>File created {timeAgo(files?.createdAt!)}</p>
+                    </div>
+                    <div className="flex gap-2 justify-center items-center">
+                        <Check className="w-4 h-4" />      <p className='text-muted-foreground '>File modified {timeAgo(files?.updatedAt!)}</p>
+                    </div>
+
+
+
+
+                </div>
+                    <div className="mt-4 flex justify-center items-center">
+                        <Link href={files?.fileUrl!}>
+
+                            <Button variant='default'>
+                        <Download className="w-4 h-4 mr-2"/>
+                                    View & Download
+                            </Button>
+                        </Link>
 
                     </div>
             </EmptyPlaceholder>
@@ -35,14 +58,14 @@ export const FileDescriptionSkeleton = () => {
     return (
 
         <EmptyPlaceholder className="bg-gradient-to-tr from-purple-400/10 rounded-md  via-transparent to-transparent/5 w-full flex justify-start ">
-         <EmptyPlaceholder.Icon name="post" />
-         <div className="flex flex-col gap-2">
-            <BodySkeleton />
-          
+            <EmptyPlaceholder.Icon name="post" />
+            <div className="flex flex-col gap-2">
+                <BodySkeleton />
 
 
-         </div>
-        
+
+            </div>
+
         </EmptyPlaceholder>
 
     )
