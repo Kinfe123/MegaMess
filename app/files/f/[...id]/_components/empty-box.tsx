855dcd54c1a91592f47ac61ed67fbfd3c03b8eaf
Kinfe123 <kinfishtech@gmail.com>
2024-03-03 22:34:48 +0300

    fix: min word update

--- app/files/f/[...id]/_components/empty-box.tsx
+++ app/files/f/[...id]/_components/empty-box.tsx
@@ -15,7 +15,7 @@ const FileDescription = async ({ file }: FilePromiseProps) => {
     return (
         <>
 
-            <EmptyPlaceholder className="bg-gradient-to-tr from-purple-400/10 rounded-md  via-transparent to-transparent/5 w-full flex justify-start ">
+            <EmptyPlaceholder className="bg-gradient-to-tr from-purple-400/10 rounded-lg  via-transparent to-transparent/5 w-full flex justify-start ">
                 <EmptyPlaceholder.Icon name="post" />
 
                 <EmptyPlaceholder.Title className="font-heading text-3xl"> <span className='text-gradient_indigo-purple font-extrabold'>{files?.name}</span></EmptyPlaceholder.Title>
@@ -25,10 +25,10 @@ const FileDescription = async ({ file }: FilePromiseProps) => {
                 <div className='flex font-urban justify-start items-start flex-col  mx-auto f'>
                     <div className="flex gap-2 justify-center items-center">
 
-                        <Check className="w-4 h-4" />  <p className='text-muted-foreground '>File created {timeAgo(files?.createdAt!)}</p>
+                        <Check className="w-4 h-4" />  <p className='text-muted-foreground '>File created <span className='font-bold'> {timeAgo(files?.createdAt!)}</span></p>
                     </div>
                     <div className="flex gap-2 justify-center items-center">
-                        <Check className="w-4 h-4" />      <p className='text-muted-foreground '>File modified {timeAgo(files?.updatedAt!)}</p>
+                        <Check className="w-4 h-4" />      <p className='text-muted-foreground '>File modified <span className="font-bold"> {timeAgo(files?.updatedAt!)} </span></p>
                     </div>
 
 
