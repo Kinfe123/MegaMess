4b3e35033dbc58ce0df2b046ff08421e290c467a
Kinfe123 <kinfishtech@gmail.com>
2024-03-05 22:36:24 +0300

    feat: fav init

--- app/(dashboard)/dashboard/favorites/page.tsx
+++ app/(dashboard)/dashboard/favorites/page.tsx
@@ -1,24 +1,33 @@
 import { getCurrentUser } from "@/lib/session"
 import { redirect } from "next/navigation"
+import { DashboardHeader } from "@/components/dashboard/header"
+import { DashboardShell } from "@/components/dashboard/shell"
+
 
 export const metadata = {
     title: "Favourite",
     description: "Manage your favourite.",
-  }
-  
+}
 
 
 
-  export async function FavouritePage() {
+
+export async function FavouritePage() {
     const user = await getCurrentUser()
-    if(!user) {
+    if (!user) {
         redirect('/login')
     }
     return (
-        <div>
+        <DashboardShell>
+            <DashboardHeader
+                heading="Favorites"
+                text="Manage account and website Favorites."
+            />
+            <div className="grid gap-10">
 
-
-        </div>
+                <p>this </p>
+            </div>
+        </DashboardShell>
     )
 
 }
