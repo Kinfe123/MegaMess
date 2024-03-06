acc95103e60fba7fe8903c33210125a9fc82e9db
Kinfe123 <kinfishtech@gmail.com>
2024-03-06 22:00:10 +0300

    prod fix

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -9,23 +9,23 @@ import { Skeleton } from "./ui/skeleton";
 import { getUserById } from "@/lib/user";
 
 
-interface PromiseProps  {
-    user: {
-        image: string | null,
-         name: string | null;
-    } & File  
+interface PromiseProps {
+  user: {
+    image: string | null,
+    name: string | null;
+  } & File
 }
 type UserAvatarProps = {
-    promise: Promise<({ user: { image: string | null; name: string | null; firstName: string | null; lastName: string | null }; } & File) | null>  
-   
+  promise: Promise<({ user: { image: string | null; name: string | null; firstName: string | null; lastName: string | null }; } & File) | null>
+
 };
 
 
-const UserAvatar = async ({promise}: UserAvatarProps) => {
-  const user  = await promise
-  const firstName = user?.user.firstName  ?? ""
-  const lastName = user?.user.lastName  ?? ""
-  const fullName = firstName  + lastName  
+const UserAvatar = async ({ promise }: UserAvatarProps) => {
+  const user = await promise
+  const firstName = user?.user.firstName ?? ""
+  const lastName = user?.user.lastName ?? ""
+  const fullName = firstName + lastName
   const initials = `${fullName?.charAt(0) ?? ""} ${lastName.charAt(0) ?? ""}`
   return (
     <div className="flex gap-2 ">
@@ -35,13 +35,13 @@ const UserAvatar = async ({promise}: UserAvatarProps) => {
       </Avatar>
       <div className='flex flex-col items-start justify-center gap-[-20px]'>
         <p className='leading-none" text-sm font-medium'>{user?.user.name}</p>
-        <React.Suspense fallback={<Skeleton className="w-7 h-3"/>}>
-        <p className='text-xs leading-none text-muted-foreground'></p>
-            {fullName.length > 0 ? `${fullName}` : "MegaMesser"}
+        <React.Suspense fallback={<Skeleton className="w-7 h-3" />}>
+          <p className='text-xs leading-none text-muted-foreground'></p>
+          {fullName.length > 0 ? `${fullName}` : "MegaMesser"}
         </React.Suspense>
       </div>
-      
-      
+
+
     </div>
 
   )
