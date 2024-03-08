6b405826e4b53fd9d0c7865bcf0cc37502b563d9
Kinfe123 <kinfishtech@gmail.com>
2024-03-08 03:13:59 +0300

    fix: loaders and listens based on ID

--- app/files/f/[...id]/_components/fav-on-preview.tsx
+++ app/files/f/[...id]/_components/fav-on-preview.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Button } from "@/components/ui/button"
 import { type User } from "@prisma/client"
-import { Heart } from "lucide-react"
+import { Heart, Loader } from "lucide-react"
 import { useSigninModal } from "@/hooks/use-signin-modal"
 import { useTransition } from "react"
 import { fileFav } from "@/actions/file-actions"
@@ -12,37 +12,42 @@ type UserProps = {
         id: string
     }
 }
-const FavIt = ({ userId , fileId , ownerId }: {userId: string, fileId: string , ownerId: string } ) => {
+const FavIt = ({ userId, fileId, ownerId }: { userId: string, fileId: string, ownerId: string }) => {
 
 
-    const [pending , startTransition] = useTransition()
+    const [pending, startTransition] = useTransition()
     const handleClick = () => {
-        startTransition(() => {
-            fileFav(ownerId , fileId ).then(() => {
-                toast({
-                    title: "Woohaa! File Added to Fav list",
-                    description: "You have successfully added a file to fav list "
-                })
+        if (userId) {
 
-            }).catch((err) => {
-                toast({
-                    title: "Something went wrong",
-                    description: 'The file has not been added to the fav list'
+            startTransition(() => {
+                fileFav(ownerId, fileId).then(() => {
+                    toast({
+                        title: "Woohaa! File Added to Fav list",
+                        description: "You have successfully added a file to fav list "
+                    })
+
+                }).catch((err) => {
+                    toast({
+                        title: "Something went wrong",
+                        description: 'The file has not been added to the fav list'
+                    })
                 })
             })
-        })
+        } else {
+            signInModal.onOpen()
+        }
     }
     const signInModal = useSigninModal()
     if (!userId) {
-        return 
-        
+        return
+
     }
 
     return (
-        <Button disabled={pending} className="flex gap-2 justify-center items-center ">
-            <Heart className="w-3 h-3"/>
+        <Button disabled={pending} onClick={handleClick} className="flex gap-2 justify-center items-center ">
+            {pending ? <Loader className="w-3 h-3 animate-spin" /> : <Heart className="w-3 h-3" />}
             <span>{userId ? 'Fav It' : ''}</span>
-            
+
         </Button>
     )
 }
