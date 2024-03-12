13df6b7c6cab00bfba23bc1a3f92ebb7c111bc0d
Kinfe123 <kinfishtech@gmail.com>
2024-03-12 22:54:10 +0300

    feat: grid adaded to cards

--- components/pricing-faq.tsx
+++ components/pricing-faq.tsx
@@ -35,7 +35,31 @@ const pricingFaqData = [
 
 export function PricingFaq() {
   return (
-    <section className="container max-w-3xl py-2">
+    <section className="relative container max-w-3xl py-2">
+      <svg
+        className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+            width={200}
+            height={200}
+            x="50%"
+            y={-1}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+          <path
+            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+            strokeWidth={0}
+          />
+        </svg>
+        <rect width="100%" height="100%" strokeWidth={0} fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)" />
+      </svg>
       <div className="mb-14 space-y-6 text-center">
         <h1 className="text-balance text-center font-heading text-3xl md:text-5xl">
           Frequently Asked Questions
