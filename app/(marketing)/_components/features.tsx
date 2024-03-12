9018d9add3ec589bb252d96b3167c15f9844c541
Kinfe123 <kinfishtech@gmail.com>
2024-03-12 14:10:26 +0300

    fix: bg-trasparent and border issue on active

--- app/(marketing)/_components/features.tsx
+++ app/(marketing)/_components/features.tsx
@@ -125,7 +125,7 @@ function Feature({
       <h3
         className={clsx(
           'mt-6 text-sm font-medium',
-          isActive ? 'text-gradient_indigo-purple  ' : 'text-slate-600',
+          isActive ? 'text-gradient_indigo-purple border-none ' : 'text-slate-600',
         )}
       >
         {feature.name}
@@ -145,7 +145,7 @@ function FeaturesMobile() {
         <div key={feature.summary}>
           <Feature feature={feature} className="mx-auto max-w-2xl" isActive />
           <div className="relative mt-10 pb-10">
-            <div className="absolute -inset-x-4 bottom-0 top-8 bg-slate-200 sm:-inset-x-6" />
+            <div className="absolute -inset-x-4 bottom-0 top-8 bg-transparent sm:-inset-x-6" />
             <div className="relative mx-auto w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
               {/* <Image
                 className="w-full"
