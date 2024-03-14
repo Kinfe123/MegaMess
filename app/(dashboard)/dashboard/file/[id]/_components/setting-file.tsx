902c979db1041a6376c3655bd712d141930e7bd9
Kinfe123 <kinfishtech@gmail.com>
2024-03-15 02:15:47 +0300

    fmt

--- app/(dashboard)/dashboard/file/[id]/_components/setting-file.tsx
+++ app/(dashboard)/dashboard/file/[id]/_components/setting-file.tsx
@@ -3,22 +3,22 @@
 import { fileDelete } from "@/actions/file-actions";
 import { Button } from "@/components/ui/button"
 import { useState, useTransition } from "react";
-import {toast} from "@/components/ui/use-toast"
+import { toast } from "@/components/ui/use-toast"
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-export default  function SettingFile({fileId}: {fileId: string}) {
-  const [pending , startTransition] = useTransition()  
+export default function SettingFile({ fileId }: { fileId: string }) {
+  const [pending, startTransition] = useTransition()
   const router = useRouter()
   const handleClick = () => {
     startTransition(() => {
-      fileDelete(fileId).then((file: { status: string, name: string | undefined }) => 
-    {
-      router.push("/dashboard")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-      toast({
-        title: "Deleted Successfully.",
-        description: `The file - ${file.name} has successfully deleted!`,
-      })}).catch((err) => {
-        console.log('THe rror is : ' , err)
+      fileDelete(fileId).then((file: { status: string, name: string | undefined }) => {
+        router.push("/dashboard")
+        toast({
+          title: "Deleted Successfully.",
+          description: `The file - ${file.name} has successfully deleted!`,
+        })
+      }).catch((err) => {
+        console.log('THe rror is : ', err)
         toast({
           title: "Something went wrong.",
           description: "There is an error while deleting the file.",
@@ -26,9 +26,9 @@ export default  function SettingFile({fileId}: {fileId: string}) {
         })
       })
     })
-      
-    }
-    return (
+
+  }
+  return (
     <div className='flex flex-col w-full  mr-auto'>
       <div className="bg-transparent border-red-500/40 border-[0.001px]  shadow sm:rounded-lg">
         <div className="px-4 py-5 sm:p-6">
@@ -36,25 +36,24 @@ export default  function SettingFile({fileId}: {fileId: string}) {
           <div className="mt-2 sm:flex sm:items-start sm:justify-between">
             <div className="max-w-xl text-sm text-gray-500">
               <p>
-               This action is undone , which delete a file permanently from file storage and sharing dashboard
+                This action is undone , which delete a file permanently from file storage and sharing dashboard
               </p>
             </div>
             <div className="mt-5 sm:ml-6 sm:mt-0 sm:flex sm:flex-shrink-0 sm:items-center">
               <Button
-              disabled={pending}
-               onClick={handleClick}
+                disabled={pending}
+                onClick={handleClick}
                 type="button"
                 variant={'destructive'}
                 className="inline-flex items-center rounded-md  px-3 py-2 text-sm font-semibold ml-auto text-white shadow-sm hover:opacity-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
-            {pending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-                 Delete File
+                {pending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                Delete File
               </Button>
             </div>
           </div>
         </div>
       </div>
-      </div>
-    )
-  }
-  
+    </div>
+  )
+}
