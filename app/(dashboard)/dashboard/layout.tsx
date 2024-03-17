de8cdea47c914f10b618c1f1cd988f706f736434
Kinfe123 <kinfishtech@gmail.com>
2024-03-17 15:15:03 +0300

    fix: bordering issue

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -7,6 +7,7 @@ import { dashboardConfig } from "@/config/dashboard"
 import { getCurrentUser } from "@/lib/session"
 import GradientImg from '../../../public/gradient.png'
 import Image from 'next/image'
+import { Separator } from "@/components/ui/separator"
 interface DashboardLayoutProps {
   children?: React.ReactNode
 }
@@ -29,7 +30,8 @@ export default async function DashboardLayout({
       <NavBar user={user} items={dashboardConfig.mainNav} scroll={false} />
 
       <div className="w-full  mx-auto my-auto pl-[16rem] pr-[25rem] z-10 py-10 grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[240px] mr-4 flex-col md:flex">
+        <aside className="hidden w-[240px] mr-4 flex-col md:flex border-r border-white/20 pr-4 ">
+   
           <DashboardNav items={dashboardConfig.sidebarNav} />
         </aside>
         <main className="flex w-full ml-10 flex-1 flex-col overflow-hidden">
