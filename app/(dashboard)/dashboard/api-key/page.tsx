18d8c643975e7481c85d4b1627206a4e4db3bf7e
Kinfe123 <kinfishtech@gmail.com>
2024-03-18 22:55:38 +0300

    fix: adding header separator

--- app/(dashboard)/dashboard/api-key/page.tsx
+++ app/(dashboard)/dashboard/api-key/page.tsx
@@ -1,11 +1,13 @@
 import { DashboardHeader } from "@/components/dashboard/header"
 import { DashboardShell } from "@/components/dashboard/shell"
+import { Separator } from "@/components/ui/separator"
 
 const APIKey = () => {
     return (
         <DashboardShell>
             <DashboardHeader heading="API Keys" text="Create an APIkey and build app on top of it.">
             </DashboardHeader>
+            <Separator />
         </DashboardShell>
 
     )
