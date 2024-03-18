9bb8b1186c2ca2fb0eddc9136e83a4712598b11a
Kinfe123 <kinfishtech@gmail.com>
2024-03-18 23:04:02 +0300

    feat: v0 inited

--- tailwind.config.js
+++ tailwind.config.js
@@ -169,5 +169,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography"), require("tailwindcss-animate")],
 }
