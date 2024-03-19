8e61d32f9c102b4c1d42b69756fe4b5b228046a3
Kinfe123 <kinfishtech@gmail.com>
2024-03-19 23:40:07 +0300

    fix: moking the endpoint

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,12 +1,25 @@
 export async function GET(req: Request) {
+    // mokin something that comes as a body - 
+    // name , description , fileUrl
+    const name = 'Test name'
+    const description = 'Test Description'
+    const fileUrl = 'https://files.edgestore.dev/syq4tyuw5ogm0mk3/publicFiles/_public/e36f81e8-e69f-4c62-91e4-a5f92563fc15.svg'
     const res = await fetch('http://localhost:3000/api/files' , {
         headers: {
             'Content-Type': 'application/json',
             'api-key': 'ad0e8fa6a2a5c31dd5b7dd8e3c1195e9',
-        }
+        },
+        body: JSON.stringify({
+            name: name,
+            description: description,
+            fileUrl: fileUrl
+
+        })
         
     })
     const result = await res.json()
+
+
     console.log(result)
 
     return new Response('Hello wrodl')
