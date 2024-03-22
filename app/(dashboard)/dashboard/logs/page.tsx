6a590ec54412a5d075d19d822fe0dbb1caaf258d
Kinfe123 <kinfishtech@gmail.com>
2024-03-23 01:22:35 +0300

    fix: logs export

--- app/(dashboard)/dashboard/logs/page.tsx
+++ app/(dashboard)/dashboard/logs/page.tsx
@@ -2,7 +2,7 @@ import { DashboardHeader } from "@/components/dashboard/header"
 import { DashboardShell } from "@/components/dashboard/shell"
 import { LogsAnalytics } from "@/components/logs-analytics"
 
-export const Logs = () => {
+const Logs = () => {
     return (
         <DashboardShell>
         <DashboardHeader heading="Logs Analytics" text="Explore all the logs and everything around you" />
