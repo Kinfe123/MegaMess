a667fae6f3239d81514c9e77a8eb494da12c5d7b
Kinfe123 <kinfishtech@gmail.com>
2024-03-24 12:56:17 +0300

    fix: logs skelton

--- app/(dashboard)/dashboard/logs/loading.tsx
+++ app/(dashboard)/dashboard/logs/loading.tsx
@@ -1,19 +1,25 @@
-import { Card , CardHeader , CardTitle , CardDescription  } from "@/components/ui/card"
+import { DashboardHeader } from "@/components/dashboard/header"
+import { DashboardShell } from "@/components/dashboard/shell"
+import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 
 const LogsSkeleton = () => {
     return (
-        <Card className="flex flex-col">
-        <CardHeader className="flex flex-row items-center gap-4">
-            <div className="grid gap-1">
-                <CardTitle><Skeleton className='w-14 h-5' /></CardTitle>
-                <CardDescription><Skeleton className="w-24 h-10" /></CardDescription>
-            </div>
 
-        </CardHeader>
+        <DashboardShell>
+            <DashboardHeader heading="Logs Analytics" text="Explore all the logs and everything around you" />
+            <Card className="flex flex-col">
+                <CardHeader className="flex flex-row items-center gap-4">
+                    <div className="grid gap-1">
+                        <CardTitle><Skeleton className='w-14 h-5' /></CardTitle>
+                        <CardDescription><Skeleton className="w-24 h-10" /></CardDescription>
+                    </div>
+
+                </CardHeader>
 
 
-    </Card>
+            </Card>
+        </DashboardShell>
     )
 }
 
