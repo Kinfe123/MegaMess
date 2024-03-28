2c8acaf99195491c0305121be63dfb27d02150b1
Kinfe123 <kinfishtech@gmail.com>
2024-03-29 02:59:37 +0300

    fix: minor issue on the ui

--- app/files/f/[...id]/_components/feedback.tsx
+++ app/files/f/[...id]/_components/feedback.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { createFeedback } from "@/actions/file-actions"
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import {
     Dialog,
     DialogContent,
@@ -14,14 +14,15 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { fileSchema } from "@/lib/validations/file"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { MessageCircleDashed } from "lucide-react"
+import { Loader2, MessageCircleDashed } from "lucide-react"
 import { useTransition } from "react"
 import { useForm } from "react-hook-form"
 import { Textarea } from "@/components/ui/textarea"
 import { toast } from "@/components/ui/use-toast"
-const Feedback = ({ fileId: string }) => {
+import { cn } from "@/lib/utils"
+const Feedback = ({ fileId }: { fileId: string }) => {
     const [isPending, startTransition] = useTransition()
-    const createFeedbackApi = createFeedback.bind(null , fileId)
+    const createFeedbackApi = createFeedback.bind(null, fileId)
     const {
         handleSubmit,
         register,
@@ -30,7 +31,7 @@ const Feedback = ({ fileId: string }) => {
         resolver: zodResolver(fileSchema),
         defaultValues: {
             name: "",
-            description:"",
+            description: "",
 
 
         },
@@ -46,19 +47,19 @@ const Feedback = ({ fileId: string }) => {
                 })
             } else {
                 toast({
-                    description: "You have createed an API Key",
+                    description: "You have submitted your feedback",
                 })
             }
         });
 
     });
-    
+
 
     return (
         <Dialog>
             <DialogTrigger asChild>
                 <Button variant="outline" className="flex items-center justify-center">
-                    <MessageCircleDashed className="w-4 h-4 mr-2"/>
+                    <MessageCircleDashed className="w-4 h-4 mr-2" />
                     Feedback</Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px] bg-purple-900/5 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(217,176,225,0.12),rgba(255,255,255,0))]">
@@ -68,23 +69,33 @@ const Feedback = ({ fileId: string }) => {
                         Make changes to your profile here. Click save when you're done.
                     </DialogDescription>
                 </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                            Name
-                        </Label>
-                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                <form onSubmit={onSubmit}>
+
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="name" className="text-right">
+                                Name
+                            </Label>
+                            <Input id="name" className="col-span-3" {...register("name")} />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="username" className="text-right">
+                                Username
+                            </Label>
+                            <Textarea id="desciption" className="col-span-3" {...register("description")} placeholder="Type your message here." />
+
+                        </div>
                     </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                            Username
-                        </Label>
-                        <Input id="username" value="@peduarte" className="col-span-3" />
-                    </div>
-                </div>
-                <DialogFooter>
-                    <Button type="submit">Save changes</Button>
-                </DialogFooter>
+                    <DialogFooter>
+                        <Button className={cn(buttonVariants())}
+                            disabled={isPending}
+                            type="submit">
+
+                            {isPending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+
+                            Submit Feedback</Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
     )
